dff8d103c215e0f91c3df9210554389e929b242d
Pedrosicilia <pedro@siblaz.com>
2018-04-19 19:41:28 -0400

    Added Some Items

--- game/items/HealthPotion.h
+++ game/items/HealthPotion.h
@@ -20,3 +20,42 @@ public:
     void use() override;
 
 };
+
+class DamagePotion : public Item {
+
+public:
+
+    DamagePotion() : Item("Elixir of Might", "Increases damage done by 10% for the duration of the fight") {};
+    void use() override;
+
+};
+
+class SwordShield : public Item {
+
+public:
+
+    SwordShield() : Item("Blunt Steel Sword & Iron Shield", "+40 Health and +25 Attack") {};
+    void use() override;
+
+};
+
+class UpperSwordShield : public Item {
+
+public:
+
+    UpperSwordShield() : Item("The Unstoppable Force & Aegis of the Kings", "+100 Health and +75 Attack") {};
+    void use() override;
+
+};
+
+class Chestlower : public Item {
+
+public:
+
+    Chestlower() : Item("Chainmail vest", "+25 Health") {};
+    void use() override;
+
+};
+
+
+
