01cfc02df0965770914078702522bc3d175702a3
Mustafa Mohamed <mustafa.mohamed@ufl.edu>
2018-04-19 18:51:59 -0400

    added setHealth, setEnergy, setMaxEnergy, setMaxHealth

--- combat/Battler.h
+++ combat/Battler.h
@@ -9,6 +9,10 @@ class Battler {
         int getMaxHealth() const;
         int getEnergy() const;
         int getMaxEnergy() const;
+        void setHealth(int health);
+        void setEnergy (int energy);
+        void setMaxHealth (int max_health);
+        void setMaxEnergy (int max_energy);
 
     private:
 
